853bdcdfeed433e4a1709e335118188de21f9948
beatriz <beatrizgama@ufrrj.com>
2024-08-17 19:41:34 -0300

    uml java

---
+++ Desafios/UML_iphone/src/Iphone.java
@@ -0,0 +1,58 @@
+
+public class Iphone implements ReprodutorMusical , AparelhoTelefonico , NavegadorInternet{
+
+	@Override
+	public void exibirPagina(String url) {
+		
+	}
+
+	@Override
+	public void adicionarNovaAba() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizarPagina() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ligar(String numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atender() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciarCorreioVoz() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tocar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pausar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selecionarMusica(String musica) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
